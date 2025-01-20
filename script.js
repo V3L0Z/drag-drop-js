1cf71a54092bfa0cab3f2deae6333fded1aafc7a
Gabriel Veloz <gabriel.veloz@fluke.com>
2025-01-20 13:04:44 -0800

    class based approach

--- script.js
+++ script.js
@@ -1,57 +1,82 @@
-const sortableList = document.getElementById("sortable-list");
-let draggedItem = null;
+// Class for sorting html cards
+class SortableList {
+  constructor(id) {
+    this.sortableList = document.getElementById(id);
+    this.draggedItem = null;
 
-sortableList.addEventListener("dragstart", (e) => {
-  if (e.target.classList.contains("draggable")) {
-    draggedItem = e.target;
-    e.target.classList.add("dragging");
+    this.initEventListeners();
   }
 
-  setTimeout(() => {
-    e.target.style.display = "none";
-  }, 0);
-});
-
-sortableList.addEventListener("dragend", (e) => {
-  if (e.target.classList.contains("draggable")) {
-    e.target.classList.remove("dragging");
-    setTimeout(() => {
-      e.target.style.display = "";
-      draggedItem = null;
-    }, 0);
+  initEventListeners() {
+    this.sortableList.addEventListener(
+      "dragstart",
+      this.onDragStart.bind(this)
+    );
+    this.sortableList.addEventListener("dragend", this.onDragEnd.bind(this));
+    this.sortableList.addEventListener("dragover", this.onDragOver.bind(this));
+    this.sortableList.addEventListener("drop", this.onDrop.bind(this));
   }
-});
 
-sortableList.addEventListener("dragover", (e) => {
-  e.preventDefault();
-  const afterElement = getDragAfterElement(sortableList, e.clientX);
-  if (afterElement == null) {
-    sortableList.appendChild(draggedItem);
-  } else {
-    sortableList.insertBefore(draggedItem, afterElement);
-  }
-});
+  onDragStart(e) {
+    if (e.target.classList.contains("draggable")) {
+      this.draggedItem = e.target;
+      e.target.classList.add("dragging");
 
-const getDragAfterElement = (container, x) => {
-  const draggableElements = [
-    ...container.querySelectorAll(".draggable:not(.dragging)"),
-  ];
-
-  return draggableElements.reduce(
-    (closest, child) => {
-      const box = child.getBoundingClientRect();
-      const offset = x - box.left - box.width / 2;
-      if (offset < 0 && offset > closest.offset) {
-        return {
-          offset: offset,
-          element: child,
-        };
-      } else {
-        return closest;
-      }
-    },
-    {
-      offset: Number.NEGATIVE_INFINITY,
+      setTimeout(() => {
+        e.target.style.display = "none";
+      }, 0);
     }
-  ).element;
-};
+  }
+
+  onDragEnd(e) {
+    if (e.target.classList.contains("draggable")) {
+      e.target.classList.remove("dragging");
+
+      setTimeout(() => {
+        e.target.style.display = "";
+        this.draggedItem = null;
+      }, 0);
+    }
+  }
+
+  onDragOver(e) {
+    e.preventDefault();
+    const afterElement = this.getDragAfterElement(e.clientX);
+    const currentItem = this.draggedItem;
+    if (afterElement == null) {
+      this.sortableList.appendChild(currentItem);
+    } else {
+      this.sortableList.insertBefore(currentItem, afterElement);
+    }
+  }
+
+  onDrop(e) {
+    e.preventDefault();
+    this.sortableList.querySelectorAll(".draggable").forEach((item) => {
+      item.style.display = "";
+    });
+  }
+
+  getDragAfterElement(x) {
+    const draggableElements = [
+      ...this.sortableList.querySelectorAll(".draggable:not(.dragging)"),
+    ];
+
+    return draggableElements.reduce(
+      (closest, child) => {
+        const { left, width } = child.getBoundingClientRect();
+        const offset = x - left - width / 2;
+
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      { offset: Number.NEGATIVE_INFINITY }
+    ).element;
+  }
+}
+
+// Usage
+const sortableList = new SortableList("sortable-list");
